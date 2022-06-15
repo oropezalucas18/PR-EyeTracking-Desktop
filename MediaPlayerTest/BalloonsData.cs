22f9c5809d7add7826c47f5144937f36c6d2c9c4
oropezalucas18 <88797809+oropezalucas18@users.noreply.github.com>
2022-06-14 21:35:56 -0400

    Proyecto de escritorio completo

---
+++ MediaPlayerTest/BalloonsData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MediaPlayerTest
+{
+    public static class BalloonsData
+    {
+        public static List<Balloon> HeatmapData { get; set; }
+        public static string FilePath { get; set; }
+        public static string FileName { get; set; }
+        public static bool IsFileSelected { get; set; }
+        public static bool IsHeatmapGenerated { get; set; }
+
+        public static List<Balloon> GetBalloons()
+        {            
+            HeatmapData = new List<Balloon>();            
+            string[] data = File.ReadAllLines(FilePath);
+            int majorX = GetXMajor();
+            int majorY = GetYMajor();
+
+            for (int i = 0; i < data.Length; i++)
+            {                                
+                HeatmapData.Add(Balloon.BalloonValues(data[i], majorX, majorY));                                    
+            }
+            
+            return HeatmapData;
+        }        
+
+        private static int GetXMajor()
+        {
+            string[] data = File.ReadAllLines(FilePath);
+            int[] dataInt = new int[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                dataInt[i] = int.Parse(data[i].Split(',')[0]);
+            }
+
+            int major = dataInt.Max();
+            
+            return major;
+        }
+
+        private static int GetYMajor()
+        {
+            string[] data = File.ReadAllLines(FilePath);
+            int[] dataInt = new int[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                dataInt[i] = int.Parse(data[i].Split(',')[1]);
+            }
+
+            int major = dataInt.Max();
+
+            return major;
+        }
+
+    }
+}
